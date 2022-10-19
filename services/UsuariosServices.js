6e6e03f51670b31556622e83cae70cd7ad136314
Luisa Nievas <luisa.nievas94@gmail.com>
2022-10-19 10:04:13 -0300

    Primeiro commit

---
+++ services/UsuariosServices.js
@@ -0,0 +1,72 @@
+const usuarios = require('../databases/usuarios.json')
+
+function listar(){
+    
+       for(let i = 0; i< usuarios.length; i++){
+        console.log(usuarios[i]);
+        console.table(usuarios[i].id);
+        console.table(usuarios[i].nome);
+        console.table(usuarios[i].email);
+       }
+}
+
+
+function salvar(arrayDeUsuarios){
+    // Seu código aqui
+}
+
+function cadastrar(objeto){
+// Seu código aqui
+}
+
+function detalhar(idUsuario){
+// Seu código aqui
+}
+
+function remover(idDoUsuarioParaRemover){
+    // Seu código aqui
+}
+
+function alterar(novosDados, idUsuario){
+    // Seu código aqui
+}
+
+function addEndereco(novoEndereco, idUsuario){
+    // Seu código aqui
+}
+
+function removerEndereco(posicaoDoEndereco, idUsuario){
+// Seu código aqui
+}
+
+function alterarEndereco(posicaoDoEndereco, novoEndereco, idUsuario){
+// Seu código aqui        
+}
+
+function addFormaDePagamento(novaFormaDePagamento, idUsuario){
+    // Seu código aqui
+}
+
+function removerFormaDePagamento(posicaoDaFormaDePagamento, idUsuario){
+    // Seu código aqui
+}
+
+function alterarFormaDePagamento(novaFormaDePagamento, posicaoDaFormaDePagamento, idUsuario){
+    // Seu código aqui
+}
+
+const UsuariosServices = {
+    cadastrar,
+    listar,
+    detalhar,
+    remover,
+    alterar,
+    addEndereco,
+    removerEndereco,
+    alteraEndereco: alterarEndereco,
+    addFormaDePagamento,
+    removerFormaDePagamento,
+    alterarFormaDePagamento
+}
+
+module.exports = UsuariosServices;
