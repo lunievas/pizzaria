0bbdb8fb50fb6cf1cb84a18fd32aea9f16838fa3
Luisa Nievas <luisa.nievas94@gmail.com>
2022-11-09 11:31:03 -0300

    Criando o html e página carrinho, colocando imagens e linkando com o servidor

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 //1- Importar o express
 const express = require('express');
+const path = require('path');
 
 
 
 //2- Criar o servidor
 const servidor = express();
 
+//Define a pasta public como sendo a pasta de arquivos estáticos
+servidor.use(express.static(path.join(__dirname, 'public')));
+
+
 //3- Definir uma rota neste servidor
 //Endereço, método, função callback ( a ação que o servidor vai realizar quando a requisição chegar)
 
-servidor.get('/usuarios', (req,res)=> {
-    console.log("chegou uma requisição");
-    // return res.send("Sua ligação é muito importante...");
+servidor.get('/', (req,res)=> {
+
     return res.sendFile(__dirname + '/views/index.html');
 } );
 
 //4- Colocar o servidor no modo "Aguardando requisição"
 
+servidor.get('/carrinho', (req,res)=>{
+
+    return res.sendFile(__dirname + '/views/carrinho.html')
+})
+
+
+
 servidor.listen(3000)
+
